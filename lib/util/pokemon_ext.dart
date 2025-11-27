ae88f7f02eb8bad037d0ed488c03e05f436a1344
dikawardaniTrainer <dikawardani.trainer@gmail.com>
2025-11-27 17:28:01 +0700

    clean up

--- lib/util/pokemon_ext.dart
+++ lib/util/pokemon_ext.dart
@@ -1,10 +1,7 @@
 
-import 'package:collection/collection.dart';
 import 'package:core/core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:pokedex/pokedex.dart';
-import 'package:sprout_pokedex/util/string_ext.dart';
 
 enum PokedexTypeColor {
   bug(Color(0xFF8CB230), Color(0XFF8BD674)),
@@ -44,70 +41,6 @@ extension AppPokemonExt on AppPokemon {
   );
 }
 
-extension PokemonExt on Pokemon {
-  String get imageUrl {
-    final stringNumber = id.toString();
-    if (id < 905) {
-      return 'https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/thumbnails/${stringNumber.padLeft(3, '0')}.png';
-    }
-    return 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$stringNumber.png';
-  }
-
-  String get pokenumber => '#${id.toString().padLeft(4, '0')}';
-  
-  PokedexTypeColor get pokedexTypeColor => PokedexTypeColor.values.firstWhere(
-        (element) => types.first.type.name.toLowerCase() == element.name,
-    orElse: () => PokedexTypeColor.unknown,
-  );
-
-  List<String> get typeNames => types.map((type) => type.type.name).toList();
-}
-
-extension ListTypeExtensions on List<Type> {
-  Map<String, double> get damageFrom {
-    final damage = <String, double>{};
-
-    forEach((type) {
-      for (var it in type.damageRelations.noDamageFrom) {
-        if (damage.containsKey(it.name)) {
-          damage[it.name] = damage[it.name]! * 0.0;
-        } else {
-          damage[it.name] = 0.0;
-        }
-      }
-
-      for (var it in type.damageRelations.halfDamageFrom) {
-        if (damage.containsKey(it.name)) {
-          damage[it.name] = damage[it.name]! * 0.5;
-        } else {
-          damage[it.name] = 0.5;
-        }
-      }
-
-      for (var it in type.damageRelations.doubleDamageFrom) {
-        if (damage.containsKey(it.name)) {
-          damage[it.name] = damage[it.name]! * 2.0;
-        } else {
-          damage[it.name] = 2.0;
-        }
-      }
-    });
-
-    return damage;
-  }
-}
-
-extension PokeSpeciesExt on PokemonSpecies? {
-  String? get flavor => this?.flavorTextEntries
-      .firstWhereOrNull((element) => element.language.name == 'en')
-      ?.flavorText
-      .replaceScapeChars();
-
-  String? get genre => this?.genera
-      .firstWhereOrNull((element) => element.language.name == 'en')
-      ?.genus;
-}
-
 extension DesignStringExtensions on String {
   PokedexTypeColor get pokemonColor => PokedexTypeColor.values.firstWhere(
         (element) => toLowerCase() == element.name,
@@ -119,73 +52,5 @@ extension DesignStringExtensions on String {
       return 'assets/$this';
     }
     return '${!kIsWeb ? 'assets/' : ''}$this';
-  }
-}
-
-extension PokemonHeightExt on int {
-  // Convert PokeAPI height units to inches
-  double get toInches {
-    // PokeAPI: 1 unit = 0.1 meters
-    // 1 meter = 39.3701 inches
-    return (this / 10.0) * 39.3701;
-  }
-
-  // Get feet and inches format
-  String get toFeetInches {
-    final totalInches = toInches;
-    final feet = totalInches ~/ 12;
-    final inches = (totalInches % 12).toStringAsFixed(1);
-    return "$feet' ${inches}\"";
-  }
-
-  // Get inches only (formatted)
-  String get toInchesFormatted {
-    return '${toInches.toStringAsFixed(1)}"';
-  }
-
-  // Get meters (for reference)
-  double get toMeters {
-    return this / 10.0;
-  }
-
-  String get toMetersFormatted {
-    return '${toMeters.toStringAsFixed(1)} m';
-  }
-
-  // Convert PokeAPI weight units to pounds
-  double get toPounds {
-    // PokeAPI: 1 unit = 0.1 kg
-    // 1 kg = 2.20462 pounds
-    return (this / 10.0) * 2.20462;
-  }
-
-  // Get pounds formatted
-  String get toPoundsFormatted {
-    return '${toPounds.toStringAsFixed(1)} lbs';
-  }
-
-  // Get kilograms (for reference)
-  double get toKilograms {
-    return this / 10.0;
-  }
-
-  String get toKilogramsFormatted {
-    return '${toKilograms.toStringAsFixed(1)} kg';
-  }
-
-  // Get both units
-  String get toBothUnits {
-    return '$toKilogramsFormatted ($toPoundsFormatted)';
-  }
-
-  // Weight category
-  String get weightCategory {
-    final lbs = toPounds;
-    if (lbs < 10) return 'Very Light';
-    if (lbs < 25) return 'Light';
-    if (lbs < 100) return 'Medium';
-    if (lbs < 500) return 'Heavy';
-    if (lbs < 1000) return 'Very Heavy';
-    return 'Extremely Heavy';
   }
 }
